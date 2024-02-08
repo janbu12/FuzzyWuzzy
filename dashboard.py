ad34fff2d5e274a62da2687757b90651277edd96
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 13:19:11 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -159,7 +159,7 @@ def pertanyaan4_10122096(orders, geolocation, df_order_item, customers, sellers)
     order_items = pd.merge(df_order_item, sellers, on='seller_id', how="inner")
     # order_items_geo  = pd.merge(order_items[['seller_id','seller_zip_code_prefix', 'seller_state']], 
     #                             geolocation, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")
-    st.dataframe(order_items_geo)
+    # st.dataframe(order_items_geo)
     # order_items_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
     # order_items_geo = order_items_geo.drop(columns=['geolocation_zip_code_prefix','geolocation_city','geolocation_state','seller_city'])
     # orders = pd.merge(orders[orders['order_status']=='delivered'], customers, on='customer_id', how="inner")

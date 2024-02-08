573d3abef18ef3b0222a779992a6a221aa20fcac
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 11:34:04 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -151,6 +151,100 @@ df_sellers = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/mai
 df_customers = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/customers_dataset.csv")
 df_geolocation = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/geolocation_dataset.csv")
 
+#Data Gathering        
+df_order_item = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/order_items_dataset.csv")
+df_order_review = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/order_reviews_dataset.csv")
+df_orders = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/orders_dataset.csv")
+df_sellers = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/sellers_dataset.csv")
+df_customers = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/customers_dataset.csv")
+df_geolocation = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/geolocation_dataset.csv")
+products = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/products_dataset.csv")
+product_category_name_translation = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/product_category_name_translation.csv")
+order_payments = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/order_payments_dataset.csv")
+order_reviews = load_data("https://raw.githubusercontent.com/janbu12/FuzzyWuzzy/main/order_reviews_dataset.csv")
+
+
+#Cleaning
+
+#Type Alteration
+df_order_item["shipping_limit_date"] = df_order_item["shipping_limit_date"].astype("datetime64[ns]")
+
+df_order_review["review_creation_date"]    = df_order_review["review_creation_date"].astype("datetime64[ns]")
+df_order_review["review_answer_timestamp"] = df_order_review["review_answer_timestamp"].astype("datetime64[ns]")
+
+df_orders["order_purchase_timestamp"]      = df_orders["order_purchase_timestamp"].astype("datetime64[ns]")
+df_orders["order_approved_at"]             = df_orders["order_approved_at"].astype("datetime64[ns]")
+df_orders["order_delivered_carrier_date"]  = df_orders["order_delivered_carrier_date"].astype("datetime64[ns]")
+df_orders["order_delivered_customer_date"] = df_orders["order_delivered_customer_date"].astype("datetime64[ns]")
+df_orders["order_estimated_delivery_date"] = df_orders["order_estimated_delivery_date"].astype("datetime64[ns]")
+
+#Tabel Merging
+products = products.join(product_category_name_translation.set_index("product_category_name"), "product_category_name", validate = "m:1")
+products["product_category_name"] = products["product_category_name_english"]
+products.drop("product_category_name_english", axis = 1, inplace = True)
+products.rename(columns = {"product_name_lenght" : "product_name_length", "product_description_lenght" : "product_description_length"}, inplace = True)
+del product_category_name_translation
+
+order_items = df_order_item.join(products.set_index("product_id"), "product_id", validate = "m:1")
+order_items.drop("product_id", axis = 1, inplace = True)
+del products
+
+order_items = order_items.join(df_sellers.set_index("seller_id"), "seller_id", validate = "m:1")
+order_items.drop("seller_id", axis = 1, inplace = True)
+del df_sellers
+
+orders = df_orders.join(df_customers.set_index("customer_id"),"customer_id", validate = "1:1")
+orders.drop(["customer_id", "customer_unique_id"], axis = 1, inplace = True)
+del df_customers
+
+#Null Value Removal
+order_items.dropna(axis = 0, how = "any", inplace = True, ignore_index = True)
+
+order_reviews.drop(["review_comment_title", "review_comment_message"], axis = 1, inplace = True)
+
+canceled_orders    = orders[orders["order_status"] == "canceled"]
+created_orders     = orders[orders["order_status"] == "created"]
+shipped_orders     = orders[orders["order_status"] == "shipped"]
+unavailable_orders = orders[orders["order_status"] == "unavailable"]
+invoiced_orders    = orders[orders["order_status"] == "invoiced"]
+processing_orders  = orders[orders["order_status"] == "processing"]
+approved_orders    = orders[orders["order_status"] == "approved"]
+
+orders.drop(orders[orders["order_status"] == "canceled"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "created"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "unavailable"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "invoiced"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "processing"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "shipped"].index, inplace = True)
+orders.drop(orders[orders["order_status"] == "approved"].index, inplace = True)
+
+orders.dropna(axis = 0, how = "any", inplace = True, ignore_index = True)
+delivered_orders = orders
+del orders
+
+approved_orders = pd.concat([approved_orders, unavailable_orders, invoiced_orders, processing_orders], ignore_index = True)
+approved_orders.drop(["order_delivered_carrier_date", "order_delivered_customer_date"], axis = 1, inplace = True)
+del unavailable_orders, invoiced_orders, processing_orders
+
+created_orders.drop(["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date"], axis = 1, inplace = True)
+shipped_orders.drop("order_delivered_customer_date", axis = 1, inplace = True)
+
+delivered = canceled_orders[canceled_orders["order_delivered_customer_date"].notnull()]
+shipped   = canceled_orders[(canceled_orders["order_delivered_carrier_date"].notnull()) & (canceled_orders["order_delivered_customer_date"].isnull())]
+approved  = canceled_orders[(canceled_orders["order_approved_at"].notnull()) & (canceled_orders["order_delivered_customer_date"].isnull()) & (canceled_orders["order_delivered_carrier_date"].isnull())]
+created   = canceled_orders[(canceled_orders["order_approved_at"].isnull()) & (canceled_orders["order_delivered_carrier_date"].isnull()) & (canceled_orders["order_delivered_customer_date"].isnull())]
+
+shipped.drop("order_delivered_customer_date", axis = 1, inplace = True)
+approved.drop(["order_delivered_customer_date", "order_delivered_carrier_date"], axis = 1, inplace = True)
+created.drop(["order_delivered_customer_date", "order_delivered_carrier_date", "order_approved_at"], axis = 1, inplace = True)
+
+delivered_orders = pd.concat([delivered_orders, delivered], ignore_index = True)
+shipped_orders   = pd.concat([shipped_orders, shipped], ignore_index = True)
+approved_orders  = pd.concat([approved_orders, approved], ignore_index = True)
+created_orders   = pd.concat([created_orders, created], ignore_index = True)
+
+del canceled_orders, delivered, shipped, approved, created
+
 with st.sidebar :
     selected = option_menu('Menu',['10122096', 'blablabla'],
     icons =["easel2", "graph-up"],

614d7bcb2f2fc8e8963bf3fb82593e6ad6aa0003
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 11:50:18 +0700

    Update dashboard.py geoanalisis 5

--- dashboard.py
+++ dashboard.py
@@ -273,6 +273,27 @@ created_orders   = pd.concat([created_orders, created], ignore_index = True)
 
 del canceled_orders, delivered, shipped, approved, created
 
+orders_geo = pd.merge(delivered_orders[delivered_orders["order_status"] != "canceled"], df_geolocation, 
+                            left_on='customer_zip_code_prefix', 
+                            right_on='geolocation_zip_code_prefix',
+                            how="inner")
+            
+orders_geo['delivery_time'] = orders_geo['order_delivered_customer_date'] - orders_geo['order_delivered_carrier_date']
+orders_geo['delivery_time'] = orders_geo['delivery_time'].dt.days
+
+orders_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
+orders_geo = orders_geo.drop(columns=['geolocation_zip_code_prefix','geolocation_city','geolocation_state','customer_city','order_status',
+                                    'order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date', 'order_delivered_customer_date',
+                                    'order_estimated_delivery_date'])
+
+order_items_geo = pd.merge(order_items[['order_id', 'seller_zip_code_prefix', 'seller_state']], df_geolocation, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")
+
+order_items_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
+order_items_geo = order_items_geo.drop(columns=['geolocation_zip_code_prefix','geolocation_city','geolocation_state' ])
+
+merge_orders_df = pd.merge(orders_geo, order_items_geo, on="order_id", how="inner")
+merge_orders_df['distance_KM'] = merge_orders_df.apply(hitung_jarak, axis=1)
+
 with st.sidebar :
     selected = option_menu('Menu',['10122096', 'blablabla'],
     icons =["easel2", "graph-up"],

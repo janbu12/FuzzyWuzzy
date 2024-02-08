b3827f37da5fc26867b113d08a508ca0b4ff51e4
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 11:44:47 +0700

    Update dashboard.py geo analisis 44

--- dashboard.py
+++ dashboard.py
@@ -282,8 +282,6 @@ with st.sidebar :
 if (selected == '10122096') :
     st.header(f"Dashboard Analisis E-Commerce oleh Mizan")
     tab1,tab2,tab3,tab4,tab5 = st.tabs(["Pertanyaan 1", "Pertanyaan 2", "Pertanyaan 3", "Pertanyaan 4", "Pertanyaan 5"])
-
-    geoanalisis(delivered_orders, df_geolocation, order_items)
     
     with tab1:
         pertanyaan1_10122096(df_orders, df_order_review)

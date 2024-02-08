a8491c59b6ee191a6ec4ef73b4c667e8abb19cea
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-09 02:31:36 +0700

    Update Mizan.py

--- Mizan.py
+++ Mizan.py
@@ -157,7 +157,7 @@ def pertanyaan4_10122096(df_geolocation, order_reviews):
     mean_distance_deliver_time_df = orders_review_filtered.groupby("delivery_time")["distance_KM"].mean()
 
     with st.container(border=True, height=400):
-    col1, col2 = st.columns([0.5,0.5])
+        col1, col2 = st.columns([0.5,0.5])
         with col1:
             st.write("Dataframe")
             st.dataframe(review_distance_df, use_container_width=True, height = 150)

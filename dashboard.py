2ccecd2113ad3c017c56696f595148532e091745
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 12:40:46 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -154,8 +154,8 @@ def hitung_jarak(row):
     seller_coords = (row['geolocation_lat_y'], row['geolocation_lng_y'])
     return geodesic(customer_coords, seller_coords).kilometers
 
-# def pertanyaan4_10122096(orders, geolocation, df_order_item, customers, sellers):
-    # order_items = pd.merge(df_order_item, sellers, on='seller_id', how="inner")
+def pertanyaan4_10122096(orders, geolocation, df_order_item, customers, sellers):
+    order_items = pd.merge(df_order_item, sellers, on='seller_id', how="inner")
     # order_items_geo = pd.merge(order_items, geolocation, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")
     # order_items_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
     # order_items_geo = order_items_geo.drop(columns=['geolocation_zip_code_prefix','geolocation_city','geolocation_state','seller_city'])
@@ -270,8 +270,8 @@ if (selected == '10122096') :
     with tab3:
         pertanyaan3_10122096(df_orders, df_order_item, df_customers, df_sellers)
 
-    # with tab4:
-    #     pertanyaan4_10122096(df_orders, df_geolocation, df_order_item, df_customers, df_sellers)
+    with tab4:
+        pertanyaan4_10122096(df_orders, df_geolocation, df_order_item, df_customers, df_sellers)
 
 elif (selected == 'blablabla'):
     st.header(f"Dashboard Analisis E-Commerce oleh blablabla")

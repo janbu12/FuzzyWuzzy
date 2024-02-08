7ba0297926970080cb1ac73e762d34eddab535c2
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 13:43:13 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -154,7 +154,7 @@ def pertanyaan3_10122096(orders, order_item, customers, sellers):
 
 def pertanyaan4_10122096(orders, order_item, customers, sellers, geolocation):
     order_items = pd.merge(order_item, sellers, on='seller_id', how='inner')
-    order_items.drop_duplicates(subset=['order_id'], keep='first', inplace=True)
+    order_items.drop_duplicates(['order_id'], keep='last', inplace=True, ignore_index = True)
     # order_items = pd.merge(order_items, df_geolocation, left_on='seller_zip_code_prefix', 
     #                     right_on='geolocation_zip_code_prefix',
     #                     how="inner", )

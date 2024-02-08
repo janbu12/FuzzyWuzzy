c34ccbe811113fb601d770c8fd2edac84f84a4f5
janbu12 <111039118+janbu12@users.noreply.github.com>
2024-02-08 12:42:03 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -136,7 +136,8 @@ def pertanyaan3_10122096(orders, order_item, customers, sellers):
 
     # Grafik Heatmap
     st.dataframe(heatmap_data)
-    
+
+    plt.figure()
     sea.heatmap(heatmap_data, cmap='coolwarm', annot=True, fmt='g')
     plt.title('Pengaruh Negara Asal antara Seller dan Customer State terhadap Jumlah Transaksi')
     plt.xlabel('Customer State')
@@ -156,8 +157,8 @@ def hitung_jarak(row):
 
 def pertanyaan4_10122096(orders, geolocation, df_order_item, customers, sellers):
     order_items = pd.merge(df_order_item, sellers, on='seller_id', how="inner")
-    # order_items_geo = pd.merge(order_items, geolocation, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")
-    # order_items_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
+    order_items_geo = pd.merge(order_items, geolocation, left_on="seller_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")
+    order_items_geo.drop_duplicates(["order_id"], keep = "last", inplace = True, ignore_index = True)
     # order_items_geo = order_items_geo.drop(columns=['geolocation_zip_code_prefix','geolocation_city','geolocation_state','seller_city'])
     # orders = pd.merge(orders[orders['order_status']=='delivered'], customers, on='customer_id', how="inner")
     # orders_geo =  pd.merge(orders, geolocation, left_on="customer_zip_code_prefix", right_on="geolocation_zip_code_prefix", how="inner")

574afdf461db50769467809bf6512893bc4f29a9
Muhammad Mizan Al Mujadid <111039118+janbu12@users.noreply.github.com>
2024-02-10 01:13:56 +0700

    Update Desi.py

--- Desi.py
+++ Desi.py
@@ -3,16 +3,15 @@ def pertanyaan1_220170203(order_items):
     st.write("<h4>Berapakah rata rata kategori terbesar dan terkecil dari jumlah foto yang dipublikasikan untuk produk?<h4>", unsafe_allow_html=True)
     category_photo = order_items[["product_category_name","product_photos_qty"]]
     category_photo = order_items.groupby("product_category_name")["product_photos_qty"].mean().sort_values(ascending = False)
-    display(category_photo.head())
-    display(category_photo.tail())
-with st.container():
-    plt.pie(category_photo.head(), labels = category_photo.head().keys())
-    plt.pie(category_photo.tail(), labels = category_photo.tail().keys())
-    grf = plt.gcf()
-    plt.close()
-    st.pyplot(grf)
 
-del grf
-
-with st.expander("Kesimpulan"):
-        st.write("Kategori terbesar dari jumlah foto yangdi publikasikan adalah fashion shoes dengan rata rata 5.229008 adapun kategori terkecil dari jumlah foto yang dipublikasikan adalah home comfort 2 dengan rata rata 1.033333") 
+    with st.container():
+        plt.pie(category_photo.head(), labels = category_photo.head().keys())
+        plt.pie(category_photo.tail(), labels = category_photo.tail().keys())
+        grf = plt.gcf()
+        plt.close()
+        st.pyplot(grf)
+    
+    del grf
+    
+    with st.expander("Kesimpulan"):
+            st.write("Kategori terbesar dari jumlah foto yangdi publikasikan adalah fashion shoes dengan rata rata 5.229008 adapun kategori terkecil dari jumlah foto yang dipublikasikan adalah home comfort 2 dengan rata rata 1.033333") 

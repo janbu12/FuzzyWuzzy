0f56ec6d3d076a4dc6ac05923970067523da52f5
amirasm22 <123288146+amirasm22@users.noreply.github.com>
2024-02-09 20:33:25 +0700

    Amira.py

--- Amira.py
+++ Amira.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import seaborn as sea
 
-def pertanyaan1_10122105(min_order):
-    st.write("Apa 10 kategori produk dengan penjualan terendah?<h4>", unsafe_allow_html=True)
-    categories_count_min_order = min_order["product_category_name"].value_counts(ascending=True)
+def pertanyaan1_10122105(order_items):
+    st.write("Apa 10 kategori produk dengan penjualan terendah?")
+    categories_count_min_order = order_items["product_category_name"].value_counts(ascending=True)
 
     min_order = categories_count_min_order.head(10)
     del categories_count_min_order
-    st.dataFrame(min_order, use_container_width = True, hide_index = True)
+    
+    st.write(min_order)
 
     plt.figure(figsize=(10, 6))
     min_order.plot(kind='bar', color='lightgreen')
@@ -17,8 +17,7 @@ def pertanyaan1_10122105(min_order):
     plt.ylabel('Order Count')
     plt.xticks(rotation=45, ha='right')  
     plt.tight_layout()
-    st.pyplot(plt.gcf())
+    st.pyplot()
 
     with st.expander("Kesimpulan"):
-        st.write("Maka kesimpulan dari hasil diatas\
-                 10 Kategori Produk dengan penjualan terendah adalah security_and_service, fashion_childrens_clothes, la_cuisine, cds_dvds_musicals, arts_and_craftmanship, fashion_sport, home_comfort_2, flowers, furniture_mattress_and_upholstery, music..")
+        st.write("Maka kesimpulan dari hasil di atas 10 Kategori Produk dengan penjualan terendah adalah security_and_service, fashion_childrens_clothes, la_cuisine, cds_dvds_musicals, arts_and_craftmanship, fashion_sport, home_comfort_2, flowers, furniture_mattress_and_upholstery, music..")

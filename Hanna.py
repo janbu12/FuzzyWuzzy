2b81088e9ec57be564bd3cd5bd4c9157df3a22ec
Jtworl08 <123296673+Jtworl08@users.noreply.github.com>
2024-02-09 18:38:04 +0700

    Add files via upload

---
+++ Hanna.py
@@ -0,0 +1,20 @@
+import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sea
+
+def pertanyaan1_10122079(order_items):
+    st.write("<h4>10 Kategori Produk Dengan Penjualan tertinggi?<h4>", unsafe_allow_html=True)
+categories_count_max_order = order_items["product_category_name"].value_counts()
+max_order = categories_count_max_order.head(10)
+del categories_count_max_order
+
+plt.figure(figsize=(10, 6))
+max_order.plot(kind='bar', color='lightblue')
+plt.title('10 Kategori Produk dengan Penjualan Tertinggi')
+plt.xlabel('Product Category')
+plt.ylabel('Order Count')
+plt.xticks(rotation=45, ha='right')  
+plt.tight_layout()
+plt.show()
+
+
+with st.expander("Kesimpulan"):
+        st.write("10 Kategori produk dengan penjualan tertinggi")

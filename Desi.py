9d7d33fdc7637afde73c13c6e3814a25b922394e
alemix13 <159552826+alemix13@users.noreply.github.com>
2024-02-10 01:47:15 +0700

    Update Desi.py

--- Desi.py
+++ Desi.py
@@ -1,5 +1,5 @@
 import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sea, streamlit as st
-def pertanyaan1_220170203(orders_items):
+def pertanyaan1_220170203(order_items):
     st.write("<h4>Berapakah rata-rata kategori terbesar dan terkecil dari jumlah poto yang dipublikasikan untuk produk<h4>",unsafe_allow_html=True)
     category_photo = order_items[["product_category_name","product_photos_qty"]]
     category_photo = category_photo.groupby("product_category_name")["product_photos_qty"].mean().sort_values(ascending = False)

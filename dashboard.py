1337e0f890606db3f39f726bb294be554c4b1be3
amirasm22 <123288146+amirasm22@users.noreply.github.com>
2024-02-09 21:01:42 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -49,7 +49,7 @@ if (selected == '10122079') :
     tab1, tab2, tab3, tab4, tab5 = st.tabs(["Pertanyaan 1", "Pertanyaan 2", "Pertanyaan 3", "Pertanyaan 4", "Pertanyaan 5"])
 
     with tab1:
-        pertanyaan1_10122079(max_order)
+        pertanyaan1_10122079(max_items=max_order)
 
 elif (selected == '10122096'):
     st.header(f"Dashboard Analisis E-Commerce oleh Mizan")

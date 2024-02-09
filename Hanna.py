932b554d0081ef304ca419fc8167c05447a85c42
Jtworl08 <123296673+Jtworl08@users.noreply.github.com>
2024-02-09 19:07:13 +0700

    Update Hanna.py

--- Hanna.py
+++ Hanna.py
@@ -17,4 +17,14 @@ def pertanyaan1_10122079(order_items):
 
 
     with st.expander("Kesimpulan"):
-    st.write("10 Kategori produk dengan penjualan tertinggi")
+    st.write("10 Kategori produk dengan penjualan tertinggi 
+    Bed bath table, 
+    Health beauty, 
+    Sports leisure, 
+    Furniture decor, 
+    Computer accessories, 
+    Housewares, 
+    Watches gifts, 
+    Telephon, 
+    Garden Tools, 
+    Auto")

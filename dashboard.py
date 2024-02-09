d434489e996b1b1a98d4b9d5e47d6db4ef66c9b5
Jtworl08 <123296673+Jtworl08@users.noreply.github.com>
2024-02-09 19:09:46 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ from streamlit_option_menu import option_menu
 from Mizan import *
 from Stefanus import *
 from Cissy import *
+from Hanna import *
 
 @st.cache_data
 def load_data(url) :
@@ -44,7 +45,10 @@ with st.sidebar :
     
 if (selected == '10122079') :
     st.header(f"Dashboard Analisis E-Commerce oleh Hana")
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Pertanyaan 1", "Pertanyaan 2", "Pertanyaan 3", "Pertanyaan 4", "Pertanyaan 5"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Pertanyaan 1")
+
+    with tab1:
+        pertanyaan1_10122079(order_items)
         
 elif (selected == '10122096'):
     st.header(f"Dashboard Analisis E-Commerce oleh Mizan")

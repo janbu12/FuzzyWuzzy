935a80c26328974112205bd2d60572a8517fe08d
alemix13 <159552826+alemix13@users.noreply.github.com>
2024-02-10 01:42:41 +0700

    Update Desi.py

--- Desi.py
+++ Desi.py
@@ -1,20 +1,21 @@
-import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sea, streamlit as st
-def pertanyaan1_220170203(order_items):
-    st.write("<h4>Berapakah rata rata kategori terbesar dan terkecil dari jumlah foto yang dipublikasikan untuk produk?<h4>", unsafe_allow_html=True)
+import pandas as pd, numpy as np, mathplotlib.pyplot as plt, seaborn as sea, streamlit as st
+def pertanyaan1_220170203(orders_items):
+    st.write("<h4>Berapakah rata-rata kategori terbesar dan terkecil dari jumlah poto yang dipublikasikan untuk produk<h4>",unsafe_allow_html=True)
     category_photo = order_items[["product_category_name","product_photos_qty"]]
-    category_photo = order_items.groupby("product_category_name")["product_photos_qty"].mean().sort_values(ascending = False)
+    category_photo = category_photo.groupby("product_category_name")["product_photos_qty"].mean().sort_values(ascending = False)
 
-    with st.container():
+    with st.cointainer():
         plt.pie(category_photo.head(), labels = category_photo.head().keys())
         grf = plt.gcf()
         st.pyplot(grf)
-
         plt.close()
+        
         plt.pie(category_photo.tail(), labels = category_photo.tail().keys())
         grf = plt.gcf()
         st.pyplot(grf)
-    
+
     del grf, category_photo
-    
     with st.expander("Kesimpulan"):
-            st.write("Kategori terbesar dari jumlah foto yangdi publikasikan adalah fashion shoes dengan rata rata 5.229008 adapun kategori terkecil dari jumlah foto yang dipublikasikan adalah home comfort 2 dengan rata rata 1.033333") 
+        st.write("Kategori Dapat disimpulkan bahwa kategori terbesar dari jumlah foto yang di publikasikan adalah fashion shoes dengan rata rata 5.229008 adapun kategori terkecil dari jumlah foto yang dipublikasikan adalah home comfort 2 dengan rata rata 1.033333")
+
+    

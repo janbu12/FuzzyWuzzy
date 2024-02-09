f75d56996e8e448ba42ef49ef498c4a1bba431ce
Stefanus Gratilio <121921500+stefanus-gratilio-05@users.noreply.github.com>
2024-02-09 17:13:52 +0700

    Update Stefanus.py

--- Stefanus.py
+++ Stefanus.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sea
+pd.options.mode.chained_assignment = None
 
 def satu(items : pd.core.frame.DataFrame):
     # Gathering

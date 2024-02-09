2663e033bfeb4f1a0ac1606caf153a51111f2e30
Jtworl08 <123296673+Jtworl08@users.noreply.github.com>
2024-02-09 19:44:24 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -48,7 +48,7 @@ if (selected == '10122079') :
     tab1, tab2, tab3, tab4, tab5 = st.tabs(["Pertanyaan 1", "Pertanyaan 2", "Pertanyaan 3", "Pertanyaan 4", "Pertanyaan 5"])
 
     with tab1:
-        pertanyaan1_10122104(order_items)
+        pertanyaan1_10122079(order_items)
         
 elif (selected == '10122096'):
     st.header(f"Dashboard Analisis E-Commerce oleh Mizan")
